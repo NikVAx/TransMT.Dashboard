c94df1a6ea8fa24c634371e02d46cbece0482e4f
NikVAx <nik.vasilenko1203@gmail.com>
2024-04-26 20:21:12 +0300

    refactor: remove errors prop

--- src/components/formInputPassword/formInputPassword.types.ts
+++ src/components/formInputPassword/formInputPassword.types.ts
@@ -1,10 +1,8 @@
 import { FormRule } from "@/shared/types";
 import { PasswordProps } from "primereact/password";
-import { FieldErrors } from "react-hook-form";
 
 export interface FormInputPasswordProps extends PasswordProps {
   name: string;
-  errors?: FieldErrors<any>;
   rules?: FormRule;
   label?: string;
 }
--- src/components/formInputText/formInputText.types.ts
+++ src/components/formInputText/formInputText.types.ts
@@ -1,10 +1,8 @@
 import { FormRule } from "@/shared/types";
 import { InputTextProps } from "primereact/inputtext";
-import { FieldErrors } from "react-hook-form";
 
 export interface FormInputTextProps extends InputTextProps {
   name: string;
-  errors?: FieldErrors<any>;
   rules?: FormRule;
   label?: string;
 }
