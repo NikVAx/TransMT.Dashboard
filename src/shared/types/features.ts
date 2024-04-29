b0379d5d21f2fba459402304048db4f077df6bfa
NikVAx <nik.vasilenko1203@gmail.com>
2024-04-29 18:15:02 +0300

    fix: add shared types; required by user store

--- src/shared/types/features.ts
+++ src/shared/types/features.ts
@@ -1,1 +1,32 @@
-export type States = "loading" | "done" | "failed";;
+export type States = "loading" | "done" | "failed";
+
+export interface IPageRequestOptions {
+  pageIndex: number;
+  pageSize: number;
+}
+
+export interface IPageOptions {
+  pageIndex: number;
+  pageSize: number;
+  pageCount: number;
+  totalCount: number;
+}
+
+export type IPaginatedRequest<T = any> = IPageRequestOptions & T;
+
+export interface IPaginatedResponse<T> extends IPageOptions {
+  items: T[];
+}
+
+export interface IGeoPoint {
+  lng: number;
+  lat: number;
+}
+
+export interface IManyDeleteOptions<TKey = string> {
+  keys: TKey[] | TKey;
+}
+
+export interface IManyDeleteRequestOptions<TKey = string> {
+  keys: TKey[];
+}
